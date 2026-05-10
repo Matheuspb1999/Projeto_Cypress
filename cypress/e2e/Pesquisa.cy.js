c345c671806610ebd0f0e395891e620f5792ccea
Matheuspb1999 <matepb13@gmail.com>
2026-05-10 00:48:08 -0300

    commit final2

--- cypress/e2e/Pesquisa.cy.js
+++ cypress/e2e/Pesquisa.cy.js
@@ -3,11 +3,10 @@ describe('Pesquisa no Google Acadêmico', () => {
   it('Deve pesquisar um tema e validar resultados', () => {
     cy.visit('/');
     cy.get('input[name="q"]')
-      .type(' Inteligência Artificial{enter}'); 
+      .type(' Inteligência Artificial{enter}');
     cy.get('#gs_res_ccl_mid')
       .should('be.visible');
-   s
-    cy.wait(3000);    
+    cy.wait(3000);
     cy.screenshot('resultado-pesquisa-google-academico');
   });
 
