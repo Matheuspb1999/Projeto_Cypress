2d998cd4eb11621adafaa5943c4768995d346d98
MatheuspBarbosa <matepb13@gmail.com>
2026-05-15 11:47:35 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://scholar.google.com.br",
     setupNodeEvents(on, config) {
     },
   },
